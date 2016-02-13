23fde3f113fe083bffd9ace780af6f0da8ccb5b3
Oskar Danielsson <oskar@smsgrupp.se>
2016-02-13 13:42:16 +0100

    Added generated authentication

---
+++ app/Http/Controllers/ChannelController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace TubeTracker\Http\Controllers;
+
+use TubeTracker\YouTube\Channel;
+use Illuminate\Http\Request;
+
+use TubeTracker\Http\Requests;
+use TubeTracker\Http\Controllers\Controller;
+
+class ChannelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $channels = Auth::user->channels;
+
+        return view('channel.index', compact('channels'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/master.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Oskar
+ * Date: 2/13/2016
+ * Time: 1:40 PM
+ */
